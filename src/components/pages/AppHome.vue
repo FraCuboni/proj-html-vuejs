13644ab5feaf169f2832fe0820ebf58e19836ea6
SimonePizziconi <simonepizziconi05@gmail.com>
2024-07-17 11:13:48 +0200

    feat: create structure section "About-us" and Section "Service"

--- src/components/pages/AppHome.vue
+++ src/components/pages/AppHome.vue
@@ -1,13 +1,16 @@
 <script>
 import { store } from '../../store';
+import AppJumbo from '../AppJumbo.vue';
 
 export default{
-
   name : 'AppHome',
-
+  components :{
+    AppJumbo
+  },
   data(){
     return{
       store,
+
     }
   },
   
@@ -16,12 +19,95 @@ export default{
 </script>
 
 <template>
-  <div>ciao</div>
+    <AppJumbo backgroundImage="/public/img/AppHome/b.png" title="Services" :subtitle="store.lorem"/>
+    <section class="about-us">
+      <div class="container container-flex">
+        <div class="card-about-us">
+          <img src="../../../public/img/AppHome/about.png" alt="">
+        </div>
+        <div class="card-about-us">
+          <div class="img-container">
+            <img class="paw" src="../../../public/img/dog-icon.png" alt="">
+            <span>About Us</span>
+          </div>
+          <h2>Welcome DogMilo Pets</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae et quas ea magnam voluptates aliquid quis perspiciatis ipsum fugiat laboriosam! Perspiciatis temporibus iusto accusantium modi vel repudiandae cum sequi velit?</p>
+        </div>
+      </div>
+    </section>
+    <section class="service">
+      <div class="container">
+        <div class="text">
+          <h6>Service</h6>
+          <h2>Dog Milo Pet Care Services For Your Best Friends.</h2>
+        </div>
+        <div class="cards-container">
+          <div class="card">
+            <img src="../../../public/img/AppHome/d-1.png" alt="">
+            <h3>Coat Handler Pets</h3>
+            <p>The standard chunk of Lorem Ipsum used since.</p>
+            <div class="button">
+              <img src="../../../public/img/right.png" alt="">
+            </div>
+
+          </div>
+        </div>
+      </div>
+      
+    </section>
+
 </template>
 
 <style lang="scss" scoped>
 @use '../../styles/partials/mixins' as *;
 @use '../../styles/partials/variables' as *;
+
+.container-flex{
+  display: flex;
+
+  .card-about-us{
+    width: 40%;
+      
+    .paw{
+      width: 50px;
+      height: 50px;
+      vertical-align: middle;
+    }  
+  }    
+}
+
+.service{
+  background-color: $light_grey;
+
+  .text{
+    text-align: center;
+  }
+
+  .cards-container{
+    display: flex;
+
+    .card{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background-color: $orange;
+      width: calc(100% / 6);
+      border-radius: 100px;
+      text-align: center;
+
+      &:hover{
+        background-color: #7f7045;
+      }
+
+      .button{
+        width: 50px;
+        height: 50px;
+        background-color: $yellow
+      }
+    }
+  }
+  
+}
 
 
 </style>
