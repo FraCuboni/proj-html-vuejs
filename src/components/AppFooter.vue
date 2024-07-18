23766e3cfcad430aef9ed694847ad406431c0500
FraCuboni <fracuboni@gmail.com>
2024-07-18 11:21:23 +0200

    feat: footer completed

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,13 +9,6 @@ export default{
 
       contactInfo : [
     {
-        section: "Get In Touch",
-        details: [
-            "Have a Question for us ?",
-            "We’ll answer your problem here"
-        ]
-    },
-    {
         section: "Support",
         details: [
             "Contact Us",
@@ -45,15 +38,6 @@ export default{
             "Returns"
         ]
     },
-    {
-        section: "Contact Me",
-        details : [
-          "4500 Mercantile plaza, Suite 300, Fort Worth, TX, 76137, USA",
-          "+0 123-456-7890",
-          "info@example.com"
-
-        ]
-    }
   ],
     }
   },
@@ -69,9 +53,35 @@ export default{
     <div class="container">
 
       <!-- list -->
+      <ul>
+        <li class="title"><h4>Get In Touch</h4></li>
+        <li class="element"><p class="small">Have a Question for us ?</p></li>
+        <li class="element"><p class="small">We’ll answer your problem here</p></li>
+        <li>
+          <div class="socials">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-pinterest"></i>
+          </div>
+        </li>
+      </ul>
+
+      
+
+      <!-- list -->
       <ul v-for="info in contactInfo">
         <li class="title"><h4>{{ info.section }}</h4></li>
-        <li v-for="element in info.details" class="element"><p class="small">{{element}}</p></li>
+        <li v-for="element in info.details" class="element"><a href="#"><p class="small">{{element}}</p></a></li>
+      </ul>
+
+      <!-- list -->
+      <ul class="icons">
+        <li class="title"><h4>Contact Me</h4></li>
+        <li class="element"><p class="small"><i class="fa-solid fa-location-arrow"></i> 4500 Mercantile plaza, Suite 300, Fort Worth, TX, 76137, USA</p></li>
+        <li class="element"><p class="small">+0 123-456-7890</p></li>
+        <li class="element"><p class="small"><i class="fa-solid fa-envelope"></i>info@example.com</p></li>
       </ul>
 
     </div>
@@ -100,6 +110,15 @@ export default{
 <style lang="scss" scoped>
 @use '../styles/partials/mixins' as *;
 @use '../styles/partials/variables' as *;
+
+a{
+  text-decoration: none;
+
+  &:hover{
+    text-decoration: underline;
+    color: white;
+  }
+}
 
 p.small {
     font-size: 15px;
@@ -199,6 +218,68 @@ section{
       }
     }
   }
+}
+
+.socials {
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: $white;
+
+  .contacts>div {
+    margin-right: 80px;
+  }
+
+  .fa-solid {
+    margin-right: 8px;
+    color: $orange;
+  }
+
+  .fa-brands {
+    padding: 6px;
+    border-radius: 3px;
+    margin-left: 2px;
+  }
+
+  .fa-facebook {
+    background-color: #303fc0;
+  }
+
+  .fa-twitter {
+    background-color: #7bd1d2;
+  }
+
+  .fa-instagram {
+    background-color: #ab27b8;
+  }
+
+  .fa-linkedin {
+    background-color: #045fd2;
+  }
+
+  .fa-pinterest {
+    background-color: #bd081b;
+  }
+}
+
+ul.icons{
+
+  li.element{
+    position: relative;
+    padding-left: 30px;
+    margin-bottom: 10px;
+
+    i{
+      position: absolute;
+      left: 0;
+      top: 0;
+      color: $orange;
+      font-size: 1.5rem;
+        
+    }
+  }
+
 }
 
 </style>
